1160c4457cfd7c2b27072aeea764c6281b8d263a
Gowtham <106096598+g-wtham@users.noreply.github.com>
2025-02-24 12:23:50 +0530

    email_integration with threading-support

--- person_detection_&_alert.py
+++ person_detection_&_alert.py
@@ -9,16 +9,41 @@ import winsound
 import pyttsx3
 import time
 import threading
+import smtplib
+from email.mime.text import MIMEText
+
 
 model = YOLO("./data/yolo11n.pt")
 cap = cv2.VideoCapture("./data/test.mp4")
 
 engine = pyttsx3.init() # TTS only need to be initialized once so it's outside the speak() function
 
+username = "curiousgowtham@gmail.com"
+password = "#"
+
+def send_email(username, password, subject="[ALERT] Person is Detected", text="A person is detected and an alert has been sent.", to_email=None):
+    if to_email is None:
+        return
+    
+    assert isinstance(to_email, list)
+
+    msg = MIMEText(text, 'plain')
+    msg['From'] = username
+    msg['To'] = ", ".join(to_email)
+    msg['Subject'] = subject
+
+    with smtplib.SMTP(host="smtp.gmail.com", port=587) as server:
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(username, to_email, msg.as_string())
+
+
 def speak():
     winsound.Beep(2500, 500)
     engine.say("Person is Detected")
+    send_email(username, password, to_email=['curiousgowtham@gmail.com', 'gowthamtechsoft@gmail.com'])
     engine.runAndWait()
+
 
 last_alert_time = 0
 while True:
@@ -39,7 +64,7 @@ while True:
                 cv2.putText(frame, "Person", (x1, y1-10), cv2.FONT_HERSHEY_COMPLEX, 0.9, (255, 0,0), 2)
                 
     current_time = time.time()
-    if person_detected and (current_time - last_alert_time > 3):
+    if person_detected and (current_time - last_alert_time > 5):
         threading.Thread(target=speak, daemon=True).start()
         last_alert_time = current_time
         
@@ -50,6 +75,3 @@ while True:
 
 cap.release()
 cap.destroyAllWindows()
-
-
-
